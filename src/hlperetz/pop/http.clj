75d1b58669e549910843ba7d7c43dbbfdbc110af
Ben Lieberman <bhlieberman93@gmail.com>
2023-07-02 08:08:29 -0600

    reformatting to use ring middleware style

--- src/hlperetz/pop/http.clj
+++ src/hlperetz/pop/http.clj
@@ -8,8 +8,13 @@
    (partial client/get (str url resource) {:headers {:x-app-token token}
                                            :basic-auth [username password]})))
 
-(defn req
-  [token username password]
-  {:token token
-   :username username
-   :password password})
+(defn wrap-req
+  ([handler]
+   (wrap-req handler {}))
+  ([handler opts]
+   (fn [token username password]
+     (->> {:token token
+           :username username
+           :password password}
+          (merge (:query-params opts))
+          (handler)))))
